787d8c9826d369e77239bb62316c944521a54d92
Vanminh2002 <vanminh200220002@Gmail.com>
2025-07-15 16:04:56 +0700

    ro chuc lai du an

---
+++ auth-service/src/main/java/com/example/authservice/config/jwt/JwtConfig.java
@@ -0,0 +1,13 @@
+package com.example.authservice.config.jwt;
+
+import io.jsonwebtoken.JwsHeader;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtConfig {
+    public String generalToken(String username) {
+
+
+        return null;
+    }
+}
--- user-service/src/main/java/com/example/userservice/entities/User.java
+++ user-service/src/main/java/com/example/userservice/entities/User.java
@@ -16,4 +16,6 @@ public class User {
     Long id;
     String name;
     String email;
+    String username;
+    String password;
 }
